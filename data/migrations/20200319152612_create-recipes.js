02da7fae77f9a10b3b985d5ea03382d02c8ffd0a
LISA_COOK <lisabpink@gmail.com>
2020-03-19 15:28:46 -0400

    LDC set up tables migration

---
+++ data/migrations/20200319152612_create-recipes.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return (knex.schema.createTable('recipes', tbl => {
+        tbl.increment('id');
+        tbl.string('recipe_name', 128).notNullable().unique();
+    })
+    .createTable('ingredients', tbl => {
+      tbl.increments('id');
+      tbl.string('ingredient_name', 128).notNullable();
+      })
+    )
+};
+
+exports.down = function(knex) {
+  
+};
