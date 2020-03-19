de19f09305ac38159dce7f9e50ae5776e7423287
LISA_COOK <lisabpink@gmail.com>
2020-03-19 15:31:15 -0400

    LDC migration table working in SQLite

--- data/migrations/20200319152612_create-recipes.js
+++ data/migrations/20200319152612_create-recipes.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
     return (knex.schema.createTable('recipes', tbl => {
-        tbl.increment('id');
+        tbl.increments('id');
         tbl.string('recipe_name', 128).notNullable().unique();
     })
     .createTable('ingredients', tbl => {
